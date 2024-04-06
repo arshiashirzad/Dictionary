0960d28494e05b595a12fb2d846e9a0d36c63571
arshiashirzad <sirzad383@gmail.com>
2024-04-06 17:13:41 +0330

    functions and method structures changed

--- main.cpp
+++ main.cpp
@@ -1,34 +1,69 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//Declaring LinkedLists
-struct Dictionary{
-
-};
- struct Synonym {
+struct Synonym {
     string value;
     Synonym* next;
 };
- struct Word{
+
+struct Word {
     string value;
     Word* next;
     Synonym* synonym;
 };
-//new word
-void addWord(){
-    string wrd;
-    cout <<"ENTER WORD:\n";
-    cin >> wrd;
-    Word* word = new Word;
-    word->value = wrd;
-    word->next= nullptr;
+
+void addWord(Word*& head, string word, string synonym) {
+    Synonym* newSynonym = new Synonym;
+    newSynonym->value = synonym;
+    newSynonym->next = nullptr;
+
+    Word* newWord = new Word;
+    newWord->value = word;
+    newWord->synonym = newSynonym;
+
+    if (head == nullptr || head->value > word) {
+        newWord->next = head;
+        head = newWord;
+        return;
+    }
+
+    Word* current = head;
+    while (current->next != nullptr && current->next->value <= word) {
+        current = current->next;
+    }
+
+    if (current->value == word) {
+        Synonym* syn = current->synonym;
+        while (syn->next != nullptr) {
+            syn = syn->next;
+        }
+        syn->next = newSynonym;
+        delete newWord;
+    } else {
+        newWord->next = current->next;
+        current->next = newWord;
+    }
 }
-void showWordInfo(Word* word){
-    while(word->next != nullptr){
-        cout<< word->value;
-        word = word->next;
+void showWordAndSynonym(Word* head){
+    Word* current = head;
+    while (current != nullptr) {
+        cout << "Word: " << current->value << endl;
+        cout<< "Synonym: ";
+        Synonym* syn = current->synonym;
+        while (syn != nullptr) {
+            cout << syn->value << " " ;
+            syn = syn->next;
+        }
+        cout << endl;
+        current = current->next;
     }
 }
 int main() {
-addWord();
+    Word* head = nullptr;
+    addWord(head, "happy", "joyful");
+    addWord(head, "happy", "content");
+    addWord(head, "happy", "glad");
+    addWord(head, "sad", "unhappy");
+    showWordAndSynonym(head);
+    return 0;
 }
