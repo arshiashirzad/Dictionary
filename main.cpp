177ec18e79e86541be48bbebf60094798ac79647
arshiashirzad <sirzad383@gmail.com>
2024-04-06 16:29:39 +0330

    structures added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+struct Synonym {
+    string value;
+    Synonym* next{};
+};
+struct Word{
+    string value;
+    Word* next{};
+    Synonym* synonym{};
+};
 
 int main() {
 
