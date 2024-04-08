f175e7c1b19fdcbb6ae6dc9b4fdf653eeac32697
arshiashirzad <sirzad383@gmail.com>
2024-04-08 12:23:17 +0330

    Dashbord added and bugs fixed

--- main.cpp
+++ main.cpp
@@ -6,18 +6,21 @@ struct Synonym {
     string value;
     Synonym *next;
 };
-
 struct Word {
     string value;
     Word *next;
     Synonym *synonym;
 };
 
-void addWord(Word *&head, string word, string synonym) {
+void addWord(Word *&head) {
     Synonym *newSynonym = new Synonym;
+    string word, synonym;
+    cout<< "Enter the word you want to add:"<<endl;
+    cin>> word;
+    cout<< "Enter the synonym you want to add:"<<endl;
+    cin>> synonym;
     newSynonym->value = synonym;
     newSynonym->next = nullptr;
-
     Word *newWord = new Word;
     newWord->value = word;
     newWord->synonym = newSynonym;
@@ -53,11 +56,10 @@ void showWordAndSynonym(Word *head) {
         return;
     }
     while (current != nullptr) {
-        cout << "Word: " << current->value << endl;
-        cout << "Synonym: ";
+        cout << "Word: " << current->value;
         Synonym *syn = current->synonym;
         while (syn != nullptr) {
-            cout << syn->value << " ";
+            cout << syn->value << "";
             syn = syn->next;
         }
         cout << endl;
@@ -65,10 +67,12 @@ void showWordAndSynonym(Word *head) {
     }
 }
 
-void deleteWord(Word *&head, string word) {
+void deleteWord(Word *&head) {
     Word *current = head;
     Word *prev = nullptr;
-
+    string word;
+    cout<< "Enter the word you want to delete:"<<endl;
+    cin>> word;
     while (current != nullptr && current->value != word) {
         prev = current;
         current = current->next;
@@ -90,14 +94,18 @@ void deleteWord(Word *&head, string word) {
         Synonym *temp = syn;
         syn = syn->next;
         delete temp;
-        cout << "Word and it's synonyms are deleted successfully!";
     }
+    cout << "Word and it's synonyms are deleted successfully!"<<endl;
     delete current;
 }
 
-void deleteSynonym(Word *&head, string word, string synonym) {
+void deleteSynonym(Word *&head) {
     Word *current = head;
-
+    string word, synonym;
+    cout<< "First enter the word:"<<endl;
+    cin>> word;
+    cout<< "Enter the synonym you want to delete:"<<endl;
+    cin>> synonym;
     while (current != nullptr && current->value != word) {
         current = current->next;
     }
@@ -124,27 +132,66 @@ void deleteSynonym(Word *&head, string word, string synonym) {
     } else {
         prev->next = syn->next;
     }
-
+    cout << "Synonym is deleted successfully!"<<endl;
     delete syn;
-
-    if (current->synonym == nullptr) {
-        deleteWord(head, word);
-    }
 }
 
-void *searchWord(Word *head, string word) {
+void *searchWord(Word *head) {
     Word *current = head;
+    string word;
+    cout<< "Enter the word you want to search:"<<endl;
+    cin>> word;
     while (current != nullptr) {
         if (current->value == word) {
-            showWordAndSynonym(current);
+            cout << "Word: " << current->value << endl;
+            Synonym* syn = current->synonym;
+            while (syn != nullptr) {
+                cout << "Synonym: " << syn->value << endl;
+                syn = syn->next;
+            }
         }
         current = current->next;
     }
-    showWordAndSynonym(nullptr);
+    cout << "Word not found in the dictionary." << endl;
 }
 
 int main() {
     Word *head = nullptr;
-
+    int cntrl=0;
+    cout<< "welcome to Dictionary"<<endl;
+    while(cntrl!= 7){
+        cout <<"1.Show dictionary"<<endl<<
+               "2.Add new word " << endl <<
+                "3.Delete a word"<< endl<<
+                "4.Delete synonym"<<endl <<
+                "5.Search Word"<<endl<<
+                "6.Save Dictionary to file"<<endl<<
+                "7.Exit"<<endl<<
+                "ENTER HERE:"<<endl;
+        cin>> cntrl;
+        switch (cntrl) {
+            case 1 :
+                showWordAndSynonym(head);
+                break;
+            case 2 :
+                addWord(head);
+                break;
+            case 3 :
+                deleteWord(head);
+                break;
+            case 4:
+                deleteSynonym(head);
+                break;
+            case 5:
+                searchWord(head);
+                break;
+            case 6:
+                deleteSynonym(head);
+                break;
+            case 7:
+                return 0;
+                break;
+        }
+    }
     return 0;
 }
